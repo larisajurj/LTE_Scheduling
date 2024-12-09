5a65d7453439f9a5bfab1636b315cf2ed1477db1
EdiOne <edione2002@gmail.com>
2024-12-10 01:05:26 +0200

    Added Network Load Checking

--- LTE_Scheduling/Source.cc
+++ LTE_Scheduling/Source.cc
@@ -37,15 +37,28 @@ void Source::handleMessage(cMessage *msg)
 
 
 {
-    ASSERT(msg == sendMessageEvent);
-    int i =  getParentModule()->getIndex();
-    cMessage *job = new cMessage("job");
-    //EV<<"Sending new packet for user "<<i<<endl;
-    send(job, "txPackets");
-
+    int nrUsers = par("usersCount");
+     int nrPackets = par("packetsPerUser");
+     int PacketLength = 1;
+     double schedulingCycle = 1.0;
+     double netwload = par("networkLoad");
+     int nrChannels = par("channels");
+     for(int i=0;i<nrPackets;i++){
+           ASSERT(msg == sendMessageEvent);
+           cMessage *job = new cMessage("job");
+           send(job, "txPackets");
+       }
+     if(simTime() >= par("MAX_Sim").doubleValue()){
+               endSimulation();
+           }
     double sendingTime;
-    sendingTime = par("sendIaTime").doubleValue();
-    scheduleAt(simTime()+ exponential(sendingTime), sendMessageEvent);
+
+     sendingTime = (nrUsers * nrPackets * PacketLength * schedulingCycle) / (netwload * nrChannels);
+
+        // Output the calculated sending time for debugging
+        EV << "Calculated Sending Time: " << sendingTime << " ms" << endl;
+
+        scheduleAt(simTime() + exponential(sendingTime), sendMessageEvent);
 
 }
 
